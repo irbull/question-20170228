ee311af6f741a7458925d8e4de5beac2de7f341a
avianey <avianey@axway.com>
2017-04-24 15:56:21 +0200

    Add a main long run for profiling purpose

---
+++ test/com/example/Profiling.java
@@ -0,0 +1,40 @@
+package com.example;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+// -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:StartFlightRecording=name=profiling,duration=60000s,filename=/home/me/profiling.jfr,settings=profile
+public class Profiling {
+
+  private static final long DURATION = 60_000; // 60s
+  private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+
+  private static volatile boolean stop = false;
+
+  public static void main(String[] args) throws InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+    executorService.submit(new Runnable() {
+      final NumberToWord service = NumberToWord.getInstance();
+      @Override
+      public void run() {
+        while (!stop) {
+          long n = ThreadLocalRandom.current().nextLong();
+          service.convert(n);
+        }
+      }
+    });
+    try {
+      Thread.sleep(DURATION);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      stop = true;
+      executorService.shutdown();
+      executorService.awaitTermination(100, TimeUnit.MILLISECONDS);
+    }
+  }
+
+}
